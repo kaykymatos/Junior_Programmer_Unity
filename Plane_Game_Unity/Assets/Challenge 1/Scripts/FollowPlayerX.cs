c49f43adfcedccfd0ef02aba7586619d18fdd52c
Kayky Matos <kayky7277@gmail.com>
2023-02-02 17:19:12 -0300

    Plane Game

---
+++ Plane_Game_Unity/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new(23.97f, 1.05f, 3.61f);
+
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Plane_Game_Unity/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    float verticalInput;
+
+    public Text scoreText;
+    int score;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(verticalInput * Time.deltaTime * Vector3.up);
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+
+        // tilt the plane up/down based on up/down arrow keys
+        // transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(verticalInput * rotationSpeed * Time.deltaTime * -Vector3.right);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacles"))
+        {
+            score--;
+            if (score <= 0)
+                scoreText.color = Color.red;
+            scoreText.text = score.ToString();
+        }
+
+        if (other.CompareTag("Point"))
+        {
+            score++;
+
+            if (score > 0)
+                scoreText.color = Color.green;
+            scoreText.text = score.ToString();
+        }
+    }
+}
